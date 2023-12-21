2a07759c35e0881e07275a34de48fbcdea85eba2
Blandine Ridolfi <blandineridolfi84@gmail.com>
2023-12-21 14:17:28 +0100

    Ajout du header et du footer du projet

--- wp-content/themes/planty/footer.php
+++ wp-content/themes/planty/footer.php
@@ -1,12 +1,18 @@
 </main>
 <?php get_sidebar(); ?>
 </div>
+
+
 <footer id="footer" role="contentinfo">
-<div id="copyright">
-&copy; <?php echo esc_html( date_i18n( __( 'Y', 'blankslate' ) ) ); ?> <?php echo esc_html( get_bloginfo( 'name' ) ); ?>
-</div>
+    <div id="politicy">
+        <a href="http://localhost/" class="politicy-footer">
+            Mentions légales
+        </a>
+    </div>
 </footer>
+
 </div>
+
 <?php wp_footer(); ?>
 </body>
 </html>
